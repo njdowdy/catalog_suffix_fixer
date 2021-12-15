9367ecd263141bfcd6334a7bbffec4517b5b1447
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-15 10:52:20 -0600

    Placeholder for parsing functions

---
+++ catalog_suffix_fixer/functions/parsing.py
@@ -0,0 +1,10 @@
+def test_func(test: str = "default"):
+    print(test)
+
+# setup functions
+# use file name to parse uploader for logging
+# use file name to parse department for logging
+
+# transform functions
+
+# output functions
