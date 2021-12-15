c2b90dea9191ef3704cdef1332b947022a4c0715
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-15 14:45:12 -0600

    Broke out watchdog process into more functions, handled pre-existing files, handled renaming files based on contents of the output folder

--- catalog_suffix_fixer/functions/monitor.py
+++ catalog_suffix_fixer/functions/monitor.py
@@ -7,8 +7,11 @@ from watchdog.events import PatternMatchingEventHandler
 
 def start_monitor(to_watch: str = 'catalog_suffix_fixer/input/'):
     # setup and define event handler
-    event_handler = PatternMatchingEventHandler("*", "", False, True)
+    event_handler = PatternMatchingEventHandler(patterns=["*.txt", "*.csv"], ignore_patterns=[],
+                                                ignore_directories=True, case_sensitive=True)
     event_handler.on_created = on_created
+    event_handler.on_modified = on_modified
+    print(f"Veranus began monitoring: {to_watch}")
     # create monitor
     observer = Observer()
     # define monitor parameters and link to event handler
@@ -25,23 +28,47 @@ def start_monitor(to_watch: str = 'catalog_suffix_fixer/input/'):
         observer.join()
 
 
-def on_created(event):
-    file = event.src_path
+def queue_existing(mypath: str):
+    for root, dirs, files in os.walk(mypath):
+        for file in files:
+            os.utime(os.path.join(root, file))
+
+
+def file_process(file: str):
     filestub = '/'.join(file.split('/')[2:])
     print(f"{filestub} has been detected.")
     # do a thing
-    #
+    # time.sleep(2)
     # check if file exists in output
     file_out = f"{file.replace('input', 'output')}"
-    if os.path.exists(f"{file_out}"):
-        while os.path.exists(f"{file_out}"):
-            file_out = rename_duplicate_file(file_out)  # if it does, rename it
-        file_out_stub = '/'.join(file_out.split('/')[2:])
-        print(f"{filestub} exists in output directory. Renamed to: {file_out_stub}")
+    file_out = check_file_exists(path=file_out, stub=filestub)
     # move to output
-    os.makedirs('/'.join(file_out.split('/')[0:-1]), exist_ok=True)
-    os.rename(f"{file}", file_out)
-    print(f"{filestub} has been processed and moved to output!")
+    move_file(path_in=file, path_out=file_out, stub=filestub)
+
+
+def move_file(path_in: str, path_out: str, stub: str):
+    os.makedirs('/'.join(path_out.split('/')[0:-1]), exist_ok=True)
+    os.rename(path_in, path_out)
+    print(f"{stub} has been processed and moved!")
+
+
+def check_file_exists(path: str, stub: str):
+    if os.path.exists(path):
+        while os.path.exists(path):
+            path = rename_duplicate_file(path)  # if it does, rename it
+        file_out_stub = '/'.join(path.split('/')[2:])
+        print(f"{stub} exists in output directory. Renamed to: {file_out_stub}")
+        return path
+    else:
+        return path
+
+
+def on_modified(event):
+    file_process(event.src_path)
+
+
+def on_created(event):
+    file_process(event.src_path)
 
 
 def rename_duplicate_file(filename: str):
--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import time
+
 import catalog_suffix_fixer as c
 import multiprocessing
 import os
@@ -8,7 +10,13 @@ if __name__ == "__main__":
     # start logging
     c.log.log_start()
     # initialize monitor tasks for each subfolder
+    max_procs = 4  # spawn up to 4 watchers
+    proc_count = 0
     for subdir in os.listdir(mypath):
-        # initialize monitors
-        multiprocessing.Process(target=c.veranus.start_monitor, args=(f"{mypath}{subdir}/", )).start()
-        # log monitor task started
+        if proc_count < max_procs:
+            # initialize monitors
+            multiprocessing.Process(target=c.veranus.start_monitor, args=(f"{mypath}{subdir}/", )).start()
+            proc_count += 1
+            # log monitor task started
+    time.sleep(0.5)
+    c.veranus.queue_existing(mypath)
