59ec9536acff48919f11e560b6b363dc4aeacdd5
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-17 08:10:33 -0600

    Implement def-main-name-main format to avoid scope shadowing

--- script.py
+++ script.py
@@ -4,18 +4,23 @@ import multiprocessing
 import os
 
 mypath = 'catalog_suffix_fixer/input/'
+max_proc = 4  # spawn up to 4 watchers
 
-if __name__ == "__main__":
+
+def main(path: str, max_processors: int) -> None:
     # start logging
     c.log.log_start()
     # initialize monitor tasks for each subfolder
-    max_procs = 4  # spawn up to 4 watchers
     proc_count = 0
-    for subdir in os.listdir(mypath):
-        if proc_count < max_procs:
+    for subdir in os.listdir(path):
+        if proc_count < max_processors:
             # initialize monitors
-            multiprocessing.Process(target=c.veranus.start_monitor, args=(f"{mypath}{subdir}/", )).start()
+            multiprocessing.Process(target=c.veranus.start_monitor, args=(f"{path}{subdir}/",)).start()
             proc_count += 1
-            c.log.log_watchdog_startup(to_watch=f"{mypath}{subdir}/")
+            c.log.log_watchdog_startup(to_watch=f"{path}{subdir}/")
     time.sleep(0.1)
-    c.veranus.queue_existing(mypath)
+    c.veranus.queue_existing(path)
+
+
+if __name__ == "__main__":
+    main(mypath, max_proc)
